056e3e1930974a0e438a91e9dc961cf208653001
Paweł Grabiński <pawelrgrabinski@gmail.com>
2024-11-12 19:06:47 +0100

    Correct redundant spaces in cookiecutter expressions

--- {{ cookiecutter.project_slug }}/.devcontainer/post-install.sh
+++ {{ cookiecutter.project_slug }}/.devcontainer/post-install.sh
@@ -2,6 +2,6 @@
 set -ex
 
 # Git configuration
-git config --global user.name " {{ cookiecutter.author }} " 
-git config --global user.email " {{ cookiecutter.author_email }} "
+git config --global user.name "{{ cookiecutter.author }}" 
+git config --global user.email "{{ cookiecutter.author_email }}"
 git config --global core.editor "code --wait"
